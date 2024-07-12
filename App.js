aca26f00cd1a3c4844d86698f5b6c036961dfd51
Amonoo <benjaminamonoowilberforce@outlook.com>
2024-07-12 22:28:46 +0000

    edit screens/ProductDetailScreen.js to contain final screen contents

--- App.js
+++ App.js
@@ -1,9 +1,6 @@
 import { CartProvider } from './contexts/CartContext';
-import { createStackNavigator } from '@react-navigation/stack';
 import DrawerNavigator from './navigation/DrawerNavigator';
 
-
-const Stack = createStackNavigator();
 
 export default function App() {
   return (
--- screens/ProductDetailScreen.js
+++ screens/ProductDetailScreen.js
@@ -1,17 +1,210 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { useRef } from 'react';
+import { View, Text, StyleSheet, Image, ScrollView } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-export default function ProducDetailScreen() {
+export default function ProductDetailScreen({ route, navigation }) {
+  const { product } = route.params;
+  
+  const scrollViewRef = useRef(null);
+
+  const scrollToTop = () => {
+    scrollViewRef.current.scrollTo({ y: 0, animated: true });
+  };
+
+  const shortTitle = (title) => {
+    return title.length > 19 ? `${title.substring(0,18)}...` : title;
+  };
+
+  const shortDescription = (description) => {
+    return description.length > 30 ?`${ description.substring(0,30)}...` : description;
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Product Detail Screen</Text>
-    </View>
+    <ScrollView ref={scrollViewRef} style={styles.container}>
+
+      <View style={styles.productItemContainer}>
+       <Image source={{ uri: product.image }} style={styles.productImage} />
+      </View>
+
+      <View style={styles.productInfo}>
+        <View style={styles.productText}>
+          <Text style={styles.productTitle}>{shortTitle(product.title)}</Text>
+
+          <Text style={styles.productDescription}>{shortDescription(product.description)}</Text>
+          <Text style={styles.productPrice}>${product.price}</Text>
+        </View>
+
+        <TouchableOpacity>
+          <Image source={require('../images/Export.png')} style={styles.exportIcon}/>
+        </TouchableOpacity>
+    
+        <View style={styles.materials}>
+          <Text style={styles.materialsTitle}>MATERIALS</Text>
+          <Text style={styles.materialsText}>We work with monitoring programmes to ensure compliance with safety, health and quality standards for our products.</Text>
+        </View>
+
+        <View style={styles.cautionsContainer}>
+          <View style={styles.cautionWrapper}>
+            <Image source={require('../images/DoNotBleach.png')} style={styles.cautionIcon}/>
+            <Text style={styles.caution}>Do not use bleach</Text>
+          </View>
+
+          <View style={styles.cautionWrapper}>
+            <Image source={require('../images/DoNotTumbleDry.png')} style={styles.cautionIcon}/>
+            <Text style={styles.caution}>Do not tumble dry</Text>
+          </View>
+
+          <View style={styles.cautionWrapper}>
+            <Image source={require('../images/DoNotWash.png')} style={styles.cautionIcon}/>
+            <Text style={styles.caution}>Dry clean with tetrachloroethylene</Text>
+          </View>
+
+          <View style={styles.cautionWrapper}>
+            <Image source={require('../images/IronLowTemperature.png')} style={styles.cautionIcon}/>
+            <Text style={styles.caution}>Iron at a maximum of 110°C/230°F</Text>
+          </View>
+        </View>
+        
+        <View style={styles.deliveryInfo}>
+          <View style={styles.delivery}>
+           <Image source={require('../images/Shipping.png')} style={styles.cautionIcon}/>
+           <Text>Free Flat Rate Shipping</Text>
+          </View>
+
+          <Text style={styles.deliveryText}>Estimated to be delivered on</Text>
+          <Text style={styles.deliveryText}>09/11/2021 - 12/11/2021</Text>
+
+          <TouchableOpacity onPress={scrollToTop}>
+           <Image source={require('../images/Up.png')} style={styles.upIcon}/>
+          </TouchableOpacity>
+        </View>
+      </View>
+
+      <TouchableOpacity style={styles.addToCartButton} >
+        <View style={styles.addToCartButtonLeft}>
+          <Image source={require('../images/Plus.png')} style={styles.plusIcon} />
+          <Text style={styles.addToCart} >ADD TO BASKET</Text>
+        </View>
+
+        <Image source={require('../images/Heart.png')} style={styles.heartIcon} />
+      </TouchableOpacity>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    padding: 16,
+  },
+  productItemContainer: {
+    height: 400,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 2,
+    backgroundColor: '#FFFF'
+  },
+  productImage: {
+    width: '100%',
+    height: 300,
+    resizeMode: 'contain',
+    marginTop: 75,
+  },
+  productInfo: {
+    padding: 10,
+    paddingTop: 0,
+    paddingRight: 45,
+    paddingBottom: 50
+  },
+  productText: {
+    marginTop: 15
+  },
+  productTitle: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    marginBottom: 2,
+  },
+  productDescription: {
+    fontSize: 15,
+    color: 'gray',
+  },
+  productPrice: {
+    fontSize: 18,
+    color: '#D2691E',
+  },
+  exportIcon: {
+    display: 'absoolute',
+    left:  290,
+    top: -60,
+    height: 18,
+    width: 18,
+  },
+  materials: {
+    marginBottom: 12
+  },
+  materialsTitle: {
+    marginBottom: 5
+  },
+  materialsText: {
+    fontSize: 14,
+    color: 'gray',
+  },
+  cautionsContainer: {
+    rowGap: 10,
+    paddingBottom: 17,
+    marginBottom: 17,
+    borderBottomWidth: 1,
+    borderBottomColor: '#D6D6D6'
+  },
+  cautionWrapper: {
+    flexDirection: 'row',
     alignItems: 'center',
+    columnGap: 10,
+  },
+  caution: {
+    fontSize: 13,
+    color: 'gray'
+  },
+  deliveryInfo: {
+
+  },
+  delivery: {
+    fontSize: 13,
+    flexDirection: 'row',
+    alignItems: 'center',
+    columnGap: 10,
+  },
+  deliveryText: {
+   marginLeft: 33,
+   fontSize: 13,
+   color: 'gray'
+  },
+  upIcon: {
+    display: 'absolute',
+    left: 282,
+    top: -58
+  },
+  addToCartButton: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: 7,
+    marginBottom: 30,
+    backgroundColor: 'gray',
+  },
+  addToCartButtonLeft: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    columnGap: 8,
+  },
+  plusIcon: {
+    color: 'with'
+  },
+  addToCart: {
+    color: '#FFFF'
+  },
+  heartIcon: {
+
   },
 });
