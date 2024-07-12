c0e50a1adefeb0fbc9f02d018f6721ae8fc70f07
Amonoo <benjaminamonoowilberforce@outlook.com>
2024-07-12 22:32:14 +0000

    edit screens/CartScreen.js to contain final screen contents

--- screens/CartScreen.js
+++ screens/CartScreen.js
@@ -1,9 +1,75 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { useContext } from 'react';
+import { View, Text, FlatList, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import { CartContext } from '../contexts/CartContext';
 
 export default function CartScreen() {
+  const { cartItems, removeFromCart } = useContext(CartContext);
+
+  const shortTitle = (title) => {
+    return title.length > 15 ? `${title.substring(0,15)}...` : title;
+  };
+
+  const shortDescription = (description) => {
+    return description.length > 30 ?`${ description.substring(0,30)}...` : description;
+  };
+
   return (
     <View style={styles.container}>
-      <Text>CartScreen Screen</Text>
+      <FlatList 
+         contentContainerStyle={styles.cont} 
+         showsVerticalScrollIndicator={false}
+         ListHeaderComponent={() => (
+          <View style={styles.checkoutLogo}>
+            <Image source={require('../images/Checkout.png')} />
+          </View>
+         )}
+        data={cartItems}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <View style={styles.cartItem}>
+           
+            <View style={styles.cartImageContainer}>
+             <Image source={{ uri: item.image }} style={styles.cartImage} />
+            </View>
+           
+            <View style={styles.cartItemLeft}>
+              <View>
+                <Text style={styles.cartTitle}>{shortTitle(item.title)}</Text>
+              </View>
+              
+              <View>
+                <Text style={styles.cartDescription}>{shortDescription(item.description)}</Text>
+              </View>
+              
+               <View>
+                <Text style={styles.cartPrice}>${item.price}</Text>
+              </View> 
+              
+            </View>
+
+              <TouchableOpacity 
+              onPress={() => removeFromCart(item.id)}
+              style={styles.removeBotton}
+              > 
+                <Image source={require('../images/remove.png')} style={styles.remove} />
+              </TouchableOpacity>
+          </View>
+        )}
+        ListFooterComponent={() => (
+          <>
+            <View  style={styles.totalContainer}>
+              <Text>ESE. TOTAL</Text>
+              <Text>{cartItems ? '$500' : '$0'}</Text>
+            </View>
+
+            <TouchableOpacity style={styles.checkoutButton} >
+              <Image source={require('../images/shoppingBag.png')} style={styles.plusIcon} />
+
+              <Text style={styles.checkout} >CHECKOUT</Text>
+            </TouchableOpacity>
+          </>
+        )}
+      />
     </View>
   );
 }
@@ -11,7 +77,76 @@ export default function CartScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    backgroundColor: '#F7F7F7',
+    padding: 16,
+  },
+  checkoutLogo: {
     alignItems: 'center',
+    justifyContent: 'center',
+    marginLeft: 150,
+    height: 50,
+    width: 30,
+    marginTop: 16,
+  },
+  cartItem: {
+    padding: 16,
+    flexDirection: 'row',
+    alignItems: 'center',
+    columnGap: 10,
+    marginBottom: -10,
+  },
+  cartImageContainer: {
+    width: 100,
+    height: 150,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 2,
+    alignItems: 'center',
+    backgroundColor: '#FFFF'
+  },
+  cartImage: {
+    width: 86, 
+    height: 120,   
+    resizeMode: 'contain',
+    marginTop: 20,
+  },
+  cartItemLeft: {
+    flex: 1,
+    justifyContent: "start",
+  },
+  cartTitle: {
+    fontSize: 17,
+  },
+  cartDescription: {
+    fontSize: 12,
+    color: 'gray',
+    marginBottom: 4
+  },
+  cartPrice: {
+    fontSize: 15,
+    color: '#D2691E',
+  },
+  removeBotton: {
+    display: 'absolute',
+    left: -25,
+    top: 55,
+  },
+  totalContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 10,
+    paddingBottom: 0,
+    marginTop: 200,
+    marginBottom: 10,
+  },
+  checkoutButton: {
+    height: 35,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'gray',
+  },
+  checkout: {
+    color: '#FFFF'
   },
 });
